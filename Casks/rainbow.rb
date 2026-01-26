9c7bca2d9c059c1679e327d6aa7b58dd1ed5e7b6
Asgeir Frimannsson <asgeirf@gmail.com>
2026-01-26 20:49:09 +0100

    cask for rainbow

---
+++ Casks/rainbow.rb
@@ -0,0 +1,24 @@
+cask "rainbow" do
+  version "0.0.1"
+  sha256 :no_check
+
+  url "https://github.com/gokapi/gokapi/releases/download/v#{version}/Rainbow-#{version}-macOS-universal.dmg"
+  name "Rainbow"
+  desc "Visual localization quality assurance tool"
+  homepage "https://github.com/gokapi/gokapi"
+
+  app "rainbow.app", target: "Rainbow.app"
+
+  # Remove quarantine bit for unsigned app
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Rainbow.app"],
+                   sudo: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/rainbow",
+    "~/Library/Preferences/com.wails.rainbow.plist",
+    "~/Library/Caches/rainbow",
+  ]
+end
