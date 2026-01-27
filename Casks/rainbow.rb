7113508e5123f8be3e00dd2195d1693a087edd28
Asgeir Frimannsson <asgeirf@gmail.com>
2026-01-27 21:37:36 +0100

    rename

--- Casks/rainbow.rb
+++ Casks/rainbow.rb
@@ -1,24 +1,24 @@
-cask "rainbow" do
+cask "bowrain" do
   version "0.1.9"
   sha256 "658c15be56f14481bbed86f735313f6e28103377da8ed30d107e2ce23e9708e1"
 
-  url "https://github.com/gokapi/gokapi/releases/download/v#{version}/Rainbow-#{version}-macOS-universal.dmg"
-  name "Rainbow"
+  url "https://github.com/gokapi/gokapi/releases/download/v#{version}/Bowrain-#{version}-macOS-universal.dmg"
+  name "Bowrain"
   desc "Visual localization quality assurance tool"
   homepage "https://github.com/gokapi/gokapi"
 
-  app "rainbow.app", target: "Rainbow.app"
+  app "bowrain.app", target: "Bowrain.app"
 
   # Remove quarantine bit for unsigned app
   postflight do
     system_command "/usr/bin/xattr",
-                  args: ["-dr", "com.apple.quarantine", "#{appdir}/Rainbow.app"],
+                  args: ["-dr", "com.apple.quarantine", "#{appdir}/Bowrain.app"],
                   sudo: false
   end
 
   zap trash: [
-    "~/Library/Application Support/rainbow",
-    "~/Library/Preferences/com.wails.rainbow.plist",
-    "~/Library/Caches/rainbow",
+    "~/Library/Application Support/bowrain",
+    "~/Library/Preferences/com.wails.bowrain.plist",
+    "~/Library/Caches/bowrain",
   ]
 end
